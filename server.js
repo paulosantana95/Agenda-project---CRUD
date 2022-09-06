22bbd6e4429c0c927f7026688e44d7d760e57b80
Paulo Santana <paulosantana.sa@gmail.com>
2022-09-06 00:46:30 -0300

    Configuring project and creating EJS template.

--- server.js
+++ server.js
@@ -2,24 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => {
-    app.emit('DATABASE is running.');
+    console.log('MongoDB is running');
+    app.emit('running');
   })
-    .catch(e => console.log(e));
-
+  .catch(e => console.log(e));
 
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const routes = require('./routes');
 const path = require('path');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
-app.use(helmet());
-
+// app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
